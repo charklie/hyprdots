3d56d54f304aee1efe0bba4f3646cf52c7b16dea
charklie <charliejohanid@gmail.com>
2024-03-09 15:59:46 +0100

    apparently, github doesnt support symlinks.

---
+++ .config/scripts/afterinstallation.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Get username
+username=$(whoami)
+
+# Make scripts executable (required for all keybindings to work)
+for file in ./*.sh; do
+    if [ -x "$file" ]; then
+        sudo chmod +r,+x $file
+        echo "$file has been made executabe."
+    fi
+done
+
+# Add user to 'input' groups (required for waybar functionality)
+if groups $username | grep &>/dev/null '\binput\b'; then
+    echo "User $username is already in the input group."
+else
+    # Add the user to the "input" group
+    sudo usermod -aG input $username
+    echo "User $username has been added to the input group."
+fi
+
+# Create symlink to this folder.
+ln -s ~/.config/scripts/ ~/.config/hypr
